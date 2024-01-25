7ecdcc4c7f04220db02fe8845891d5a6639fc38e
Lucas Monteiro <lucasmmnm@gmail.com>
2024-01-25 13:46:44 -0300

    Refactory modais

--- app.js
+++ app.js
@@ -1,52 +1,45 @@
 class Despesa {
-    constructor(ano, mes, dia, tipo, descricao, valor) {
+	constructor(ano, mes, dia, tipo, descricao, valor) {
+		this.ano = ano
+		this.mes = mes
+		this.dia = dia
+		this.tipo = tipo
+		this.descricao = descricao
+		this.valor = valor
+	}
 
-        this.ano = ano
-        this.mes = mes
-        this.dia = dia
-        this.tipo = tipo
-        this.descricao = descricao
-        this.valor = valor
-
-    }
-    
-    validarDados() {
-        for(let i in this) {
-            if(this[i] === undefined || this[i] === '' || this[i] === null) {
-                return false
-            }
-        }
-        return true
-    }
+	validarDados() {
+		for(let i in this) {
+			if(this[i] == undefined || this[i] == '' || this[i] == null) {
+				return false
+			}
+		}
+		return true
+	}
 }
 
 class Bd {
 
-    constructor() {
+	constructor() {
+		let id = localStorage.getItem('id')
 
-        let id = localStorage.getItem('id')
+		if(id === null) {
+			localStorage.setItem('id', 0)
+		}
+	}
 
-        if(id === null) {
-            localStorage.setItem('id', 0)
-        }
-        
-    }
+	getProximoId() {
+		let proximoId = localStorage.getItem('id')
+		return parseInt(proximoId) + 1
+	}
 
-    getProximoId() {
+	gravar(d) {
+		let id = this.getProximoId()
 
-        let proximoId = localStorage.getItem('id') 
-        return parseInt(proximoId) + 1
-    }
+		localStorage.setItem(id, JSON.stringify(d))
 
-    gravar(d) {
-
-        let id = this.getProximoId()
-
-        localStorage.setItem(id, JSON.stringify(d))
-
-        localStorage.setItem('id', id)
-
-    }
+		localStorage.setItem('id', id)
+	}
 }
 
 let bd = new Bd()
@@ -54,29 +47,43 @@ let bd = new Bd()
 
 function cadastrarDespesa() {
 
-    let ano = document.getElementById('ano')
-    let mes = document.getElementById('mes')
-    let dia = document.getElementById('dia')
-    let tipo = document.getElementById('tipo')
-    let descricao = document.getElementById('descricao')
-    let valor = document.getElementById('valor')
+	let ano = document.getElementById('ano')
+	let mes = document.getElementById('mes')
+	let dia = document.getElementById('dia')
+	let tipo = document.getElementById('tipo')
+	let descricao = document.getElementById('descricao')
+	let valor = document.getElementById('valor')
 
-    let despesa = new Despesa(
-        ano.value,
-        mes.value,
-        dia.value,
-        tipo.value,
-        descricao.value,
-        valor.value
-    )
+	let despesa = new Despesa(
+		ano.value, 
+		mes.value, 
+		dia.value, 
+		tipo.value, 
+		descricao.value,
+		valor.value
+	)
 
-    if(despesa.validarDados()) {
-        bd.gravar(despesa)
-        //dialog de sucesso
-        $('#sucessoGravacao').modal('show')
-    } else {
-        //dialog de erro
-        $('#erroGravacao').modal('show')
-    }
-    
+
+	if(despesa.validarDados()) {
+		//bd.gravar(despesa)
+
+		document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso'
+		document.getElementById('modal_titulo_div').className = 'modal-header text-success'
+		document.getElementById('modal_conteudo').innerHTML = 'Despesa foi cadastrada com sucesso!'
+		document.getElementById('modal_btn').innerHTML = 'Voltar'
+		document.getElementById('modal_btn').className = 'btn btn-success'
+
+		//dialog de sucesso
+		$('#modalRegistraDespesa').modal('show') 
+	} else {
+		
+		document.getElementById('modal_titulo').innerHTML = 'Erro na inclusão do registro'
+		document.getElementById('modal_titulo_div').className = 'modal-header text-danger'
+		document.getElementById('modal_conteudo').innerHTML = 'Erro na gravação, verifique se todos os campos foram preenchidos corretamente!'
+		document.getElementById('modal_btn').innerHTML = 'Voltar e corrigir'
+		document.getElementById('modal_btn').className = 'btn btn-danger'
+
+		//dialog de erro
+		$('#modalRegistraDespesa').modal('show') 
+	}
 }
